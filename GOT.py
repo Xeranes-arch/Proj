12e9e5eff21b9dd71d2314a9660ac82d4f7c3eb2
MC-Daniel <daniel.mec@gmx.at>
2023-01-18 23:30:37 +0100

    lolcat

--- GOT.py
+++ GOT.py
@@ -2,7 +2,7 @@ import pymunk
 import pymunk.pygame_util
 import pygame
 import random
-
+from pymunk.vec2d import Vec2d
 
 def main():
     print("This is GOT. \nYou place the ball, set its launch direction and initial speed as well as reset by left clicking. \nGot it? Let's go. Press Enter to start:")
@@ -15,10 +15,13 @@ def main():
 class World:
 
     def __init__(self):
+        # define screen
+        size = (1280, 720)
+        pts = [(0, 0), (size[0], 0), (size[0], size[1]), (0, size[1])]
+        self.screen = pygame.display.set_mode(size)
+        # create space
         self.space = pymunk.Space()
         self.space.gravity = (0, 981)
-        size = (1080, 720)
-        pts = [(0, 0), (size[0], 0), (size[0], size[1]), (0, size[1])]
         # make world boundaries
         for i in range(4):
             segment = pymunk.Segment(
@@ -27,62 +30,81 @@ class World:
             segment.friction = 0.999
             self.space.add(segment)
         # make target
-        t1 = pymunk.Segment(self.space.static_body, (720, 720), (720, 320), 2)
+        t1 = pymunk.Segment(self.space.static_body, (1020, 720), (1020, 320), 2)
         t1.elasticity = 0.99999
-        t2 = pymunk.Segment(self.space.static_body, (770, 720), (770, 320), 2)
+        t1.friction = 0.999
+        t2 = pymunk.Segment(self.space.static_body, (1070, 720), (1070, 320), 2)
         t2.elasticity = 0.99999
+        t2.friction = 0.999
         self.space.add(t1, t2)
-        self.screen = pygame.display.set_mode(size)
+        # ball
         self.body = None
-        self.circle = None
+        self.ball = None
 
     def run(self):
+        """Well. It runs the game."""
         draw_options = pymunk.pygame_util.DrawOptions(self.screen)
-
-        self.screen.fill((210, 210, 210))
-        self.space.debug_draw(draw_options)
-        pygame.display.update()
-
-        run = True
+        # loop parameters
+        n = 0
         update = False
+        winable = False
+        run = True
         while run:
+            self.screen.fill((210, 210, 210))
+            self.space.debug_draw(draw_options)
+            pygame.display.update()
             for event in pygame.event.get():
+                # quit
                 if event.type == pygame.QUIT:
                     run = False
-                n = 0
+                # mouseclick; first, second, third, fourth
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
                     if n == 0:
                         self.place_ball(pos)
-                        n = n + 1
+                        winable = True
+                        n += 1
                     elif n == 1:
                         self.set_dirandvel(pos)
                         update = True
-                    else:
+                        n += 1
+                    elif n == 3:
                         self.reset()
                         update = False
                         n = 0
+            # only run once user launches
             if update:
-                self.space.step(0.001)
-
-            
-    def simulate(self):
-        self.space.step(0.001)
+                self.space.step(0.005)
+                if 1020 < (self.body.position)[0] < 1070 and self.body.position[1] > 696 and winable:
+                    print("You win! \nGo again if you like.")
+                    winable = False
 
     def place_ball(self, pos):
+        """places ball at given click position"""
         self.body = pymunk.Body(mass=1, moment=10)
         self.body.position = (pos)
-        self.circle = pymunk.Circle(self.body, radius=30)
-        self.circle.elasticity = 0.95
-        self.space.add(self.body, self.circle)
-        pass
+        self.ball = pymunk.Circle(self.body, radius=20)
+        self.ball.elasticity = 0.2
+        self.ball.friction = 0.5
+        self.space.add(self.body, self.ball)
 
     def set_dirandvel(self, pos):
-        pass
+        """should draw arrow to click and wait """
+        p0 = self.body.position
+        x, y = pos
+        p1 = Vec2d(x=float(x), y=float(y))
+        impulse = 10 * (p1 - p0)
+        self.body.apply_impulse_at_local_point(impulse)
+
+
 
     def reset(self):
-        
-        pass
+        """removes previous ball"""
+        self.space.remove(self.body)
+        self.space.remove(self.ball)
+
+    def win(self):
+        """should check if the player has scored, say so, end the game (and prompt play again)"""
 
 
 if __name__ == "__main__":
